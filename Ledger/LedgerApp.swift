f94243af151e5fc7a2698deeec41ff646c1583d5
Ashwin <ashwin.raikar77@gmail.com>
2024-10-02 23:19:29 -0400

    Initial Commit

---
+++ Ledger/LedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LedgerApp.swift
+//  Ledger
+//
+//  Created by Ashwin Raikar on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct LedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
