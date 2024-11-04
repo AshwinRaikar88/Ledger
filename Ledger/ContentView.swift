0cda698c98815e841cfe6dadac28040236a0761a
Ashwin <ashwin.raikar77@gmail.com>
2024-11-04 18:30:03 -0500

    Application created with animations.

--- Ledger/ContentView.swift
+++ Ledger/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+    
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
+
+    @State private var id = 1
+    @State private var selected = "Golf"
+    
+    let goldenRatio: CGFloat = 1.618
+    let w: CGFloat = 360
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        let h = w / goldenRatio
+        
+        VStack{
+            HStack{
+                Text("LEGDER")
+                    .font(.largeTitle.bold()).padding()
+                Spacer()
+            }
+            
+            VStack{
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.black, lineWidth: 10)
+                    .fill(colors.randomElement() ?? .blue).padding()
+                    .frame(width: w, height: h)
+                    .shadow(radius: 5)
+                    .overlay(
+                        Image(systemName: "figure.\(selected.lowercased())")
+                            .font(.system(size: 64))
+                            .foregroundColor(.white)
+                        
+                    )
+                
+                Text("\(selected)!")
+                    .font(.title)
+            }
+            .transition(.slide)
+            .id(id)
+            
+            Spacer()
+            
+            Button("CLICK") {
+                withAnimation (.easeInOut(duration: 0.2)){
+                        selected = activities.randomElement() ?? "Archery"
+                        id += 1
+                    }
+            }
+            .buttonStyle(.borderedProminent)
+            
         }
-        .padding()
     }
 }
 
